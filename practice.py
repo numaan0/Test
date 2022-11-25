51f8cd10ea72e2118aa0b176ac4bd47b6642fcb7
Numan Nayeem <72100315+numaan0@users.noreply.github.com>
2022-11-26 00:37:10 +0530

    first commit

---
+++ practice.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+
+req = Request("https://ful.io/")
+html_page = urlopen(req)
+
+soup = BeautifulSoup(html_page, "lxml")
+
+links = []
+socialLinks="Social Links \n"
+mails="Mail/s \n"
+contact= "Contact Number"
+for link in soup.findAll('a'):
+    links.append(link.get('href'))
+    
+for i in links:
+    if "linked" in i or "facebook" in i:
+        socialLinks+=i+"\n"
+    if "mailto" in i:
+        mails+=i+"\n"
+    if "tel" in i:
+        contact+=i+"\n"
+
+print(socialLinks)
+print(mails)
+print(contact.replace('tel',""))
